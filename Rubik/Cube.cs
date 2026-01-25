80e27103eb91d42dec2a815bda2c93c2cc02a4c4
mathe-man <mathieumousset6@gmail.com>
2026-01-25 21:54:57 +0100

    Add face name when printing it

--- Rubik/Cube.cs
+++ Rubik/Cube.cs
@@ -131,6 +131,7 @@ public class Cube
     
     public void PrintFace(Face face)
     {
+        Console.WriteLine($"'{face}' face:");
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
